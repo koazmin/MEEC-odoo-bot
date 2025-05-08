bbc3ed6af67c6507cd3418ebcd58aac3e4f90b71
koazmin <89733430+koazmin@users.noreply.github.com>
2025-05-08 21:52:11 +0630

    Add files via upload

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function sendMessage() {
   userInput.value = "";
 
   const response = await fetch(
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + API_KEY,
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
